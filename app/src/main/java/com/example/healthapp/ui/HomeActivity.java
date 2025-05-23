8ce9d6143ed70f8a090d976ef7670c520387e970
kjhh2605 <2171168@hansugn.ac.kr>
2025-05-23 13:16:56 +0900

    홈화면 탭 기능 추가

--- app/src/main/java/com/example/healthapp/ui/HomeActivity.java
+++ app/src/main/java/com/example/healthapp/ui/HomeActivity.java
@@ -1,18 +1,48 @@
 package com.example.healthapp.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.healthapp.ui.workout.WorkoutFragment;
+import com.example.healthapp.ui.home.HomeFragment;
 import com.example.healthapp.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private TextView tabHome, tabRecord, tabSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        tabHome = findViewById(R.id.tab_home);
+        tabRecord = findViewById(R.id.tab_record);
+        tabSearch = findViewById(R.id.tab_search);
+
+        // 처음 화면에 HomeFragment 표시
+        loadFragment(new WorkoutFragment());
+
+        tabHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HomeFragment());
+            }
+        });
+        tabRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new WorkoutFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
